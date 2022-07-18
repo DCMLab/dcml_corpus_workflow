acc9174b69e405f0475edf6a929c04f0819892e0
marcelmmc <marcel.morancalderon@epfl.ch>
2022-07-18 13:55:26 +0200

    prebuilt img python

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,8 @@ time=$(date)
 echo "$HOME"
 echo "::set-output name=time::$time"
 
-pyv_="$(python -V 2>&1)"
-echo "$pyv_"
-
 pyv="$(python3 -V 2>&1)"
 echo "$pyv"
-
 ls "$GITHUB_WORKSPACE"
+pip install ms3
+ms3 -h
