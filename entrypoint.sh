59f70934e961a5c2ecab7a172da220f3750d0ea1
marcelmmc <marcel.morancalderon@epfl.ch>
2022-07-18 14:36:35 +0200

    testing entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@ echo "::set-output name=time::$time"
 pyv="$(python3 -V 2>&1)"
 echo "$pyv"
 ls "$GITHUB_WORKSPACE"
+ls "${GITHUB_WORKSPACE}/main"
+ls "${GITHUB_WORKSPACE}/main/MS3/"
 pip install ms3
 ms3 -h
-ms3 extract "${GITHUB_WORKSPACE}/main/MS3/*.mscx" -M -N -X -D
+ms3 extract "${GITHUB_WORKSPACE}/main/MS3/sample.mscx" -M -N -X -D
