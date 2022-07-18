0210cabf54ec29b62ddeb6ac5064d329962bdec7
marcelmmc <marcel.morancalderon@epfl.ch>
2022-07-18 13:28:29 +0200

    getting alpine python v

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,11 @@ echo "Hello $1"
 time=$(date)
 echo "$HOME"
 echo "::set-output name=time::$time"
+
+pyv_="$(python -V 2>&1)"
+echo "$pyv_"
+
+pyv="$(python3 -V 2>&1)"
+echo "$pyv"
+
 ls "$GITHUB_WORKSPACE"
