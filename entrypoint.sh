3ae2d2e38bd5ee4545cbe5336cd7aef0e433ea86
marcelmmc <marcel.morancalderon@epfl.ch>
2022-07-18 14:38:11 +0200

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,4 @@ ls "${GITHUB_WORKSPACE}/main"
 ls "${GITHUB_WORKSPACE}/main/MS3/"
 pip install ms3
 ms3 -h
-ms3 extract "${GITHUB_WORKSPACE}/main/MS3/sample.mscx" -M -N -X -D
+ms3 extract -f "${GITHUB_WORKSPACE}/main/MS3/sample.mscx" -M -N -X -D
