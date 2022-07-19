22572f69528e5ae8c04a1973ae61a504ef544e60
marcelmmc <marcel.morancalderon@epfl.ch>
2022-07-19 19:43:39 +0200

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
-#!/bin/bash -l
+#!/bin/bash
 
 echo "Hello $1"
-echo $option
-
 time=$(date)
 echo "$HOME"
 echo "::set-output name=time::$time"
@@ -18,14 +16,14 @@ ms3 -h
 ls "${HOME}"
 cd "${GITHUB_WORKSPACE}/main"
 
-if [[ "$1" == "extract" ]]; then
+if [ "$1" == "extract" ] then
   #statements
   ms3 extract -f "${GITHUB_WORKSPACE}/files_modified.json" -M -N -X -D
   ms3 extract -f "${GITHUB_WORKSPACE}/files_added.json" -M -N -X -D
-elif [[ "$1" == "check"  ]]; then
+elif [ "$1" == "check"  ] then
   #statements
   ms3 check -f "${GITHUB_WORKSPACE}/files_modified.json" --assertion
-elif [[  "$1" == "compare" ]]; then
+elif [  "$1" == "compare" ] then
   #statements
   echo "TO-DO"
 fi
