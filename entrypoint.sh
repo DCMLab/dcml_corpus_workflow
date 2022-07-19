698118b49db644104eab0c110aaed769674c34a6
marcelmmc <marcel.morancalderon@epfl.ch>
2022-07-19 16:52:26 +0200

    check yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -l
 
+
+
 echo "Hello $1"
 time=$(date)
 echo "$HOME"
@@ -15,5 +17,15 @@ ms3 -h
 
 ls "${HOME}"
 cd "${GITHUB_WORKSPACE}/main"
-ms3 extract -f "${GITHUB_WORKSPACE}/files_modified.json" -M -N -X -D
-ms3 extract -f "${GITHUB_WORKSPACE}/files_added.json" -M -N -X -D
+
+if [[ "$1" == "extract" ]]; then
+  #statements
+  ms3 extract -f "${GITHUB_WORKSPACE}/files_modified.json" -M -N -X -D
+  ms3 extract -f "${GITHUB_WORKSPACE}/files_added.json" -M -N -X -D
+elif [[ "$1" == "check"  ]]; then
+  #statements
+  ms3 check -f ${GITHUB_WORKSPACE}/files_modified.json --assertion
+elif [[  "$1" == "compare" ]]; then
+  #statements
+  echo "TO-DO"
+fi
