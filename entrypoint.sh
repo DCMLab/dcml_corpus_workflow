a07a790c573e3acd9b6a0735f8d7ed961703607a
marcelmmc <marcel.morancalderon@epfl.ch>
2022-07-23 17:07:05 +0200

    cleaning code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-echo "Hello $1"
-time=$(date)
-echo "$HOME"
-echo "::set-output name=time::$time"
-
-pyv="$(python3 -V 2>&1)"
-echo "$pyv"
-ls "$GITHUB_WORKSPACE"
-ls "${GITHUB_WORKSPACE}/main"
-ls "${GITHUB_WORKSPACE}/main/MS3/"
+echo "Argument being passed: $1"
+echo "Executing: pip install ms3==0.4.11"
 pip install ms3==0.4.11
+echo "Executing: ms3 -h"
 ms3 -h
-
-ls "${HOME}"
+echo "Executing: cd ${GITHUB_WORKSPACE}/main"
 cd "${GITHUB_WORKSPACE}/main"
 
 if [ "$1" == "extract" ]; then
+  echo "Executing: ms3 extract -f ${GITHUB_WORKSPACE}/files_modified.json -M -N -X -D"
   ms3 extract -f "${GITHUB_WORKSPACE}/files_modified.json" -M -N -X -D
+  echo "Executing: ms3 extract -f ${GITHUB_WORKSPACE}/files_added.json -M -N -X -D"
   ms3 extract -f "${GITHUB_WORKSPACE}/files_added.json" -M -N -X -D
 elif [ "$1" == "check"  ]; then
+  echo "Executing: ms3 check -f ${GITHUB_WORKSPACE}/files_modified.json --assertion"
   ms3 check -f "${GITHUB_WORKSPACE}/files_modified.json" --assertion
 elif [  "$1" == "compare" ]; then
+  echo "Executing: ms3 compare -f ${GITHUB_WORKSPACE}/files_modified.json"
   ms3 compare -f "${GITHUB_WORKSPACE}/files_modified.json"
 fi
