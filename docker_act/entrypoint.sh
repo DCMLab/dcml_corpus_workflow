0a404e7b6035c5f7fa7cf84659b7896bcc05891a
marcelmmc <marcel.morancalderon@epfl.ch>
2022-08-08 19:14:26 +0200

    Update entrypoint.sh

--- docker_act/entrypoint.sh
+++ docker_act/entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-pushing_files() {
-  echo "Pushing files"
-  cd "${GITHUB_WORKSPACE}/main"
-  git add -A
-  git commit -m "$1"
-  # git push
-}
-
 echo "Argument being passed: $1"
 # echo "Executing: pip install ms3==0.4.11"
 # pip install ms3==0.4.11
@@ -21,18 +13,22 @@ if [ "$1" == "extract" ]; then
   echo "Executing: ms3 extract -f ${GITHUB_WORKSPACE}/files_added.json -M -N -X -D"
   ms3 extract -f "${GITHUB_WORKSPACE}/files_added.json" -M -N -X -D
 
-  # git add -A
-  # git commit -m "Automatically added TSV files from parse with ms3"
-  # git push
-  pushing_files "Automatically added TSV files from parse with ms3"
+
+  echo "Pushing files"
+  git add -A
+  git commit -m "Automatically added TSV files from parse with ms3"
+  git push
 elif [ "$1" == "check"  ]; then
   echo "Executing: ms3 check -f ${GITHUB_WORKSPACE}/files_modified.json --assertion"
   ms3 check -f "${GITHUB_WORKSPACE}/files_modified.json" --assertion
 elif [  "$1" == "compare" ]; then
   echo "Executing: ms3 compare -f ${GITHUB_WORKSPACE}/files_modified.json"
   ms3 compare -f "${GITHUB_WORKSPACE}/files_modified.json"
-
   git config --global user.name "github-actions[bot]"
   git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
-  pushing_files "Added comparison files for review"
+
+  echo "Pushing files"
+  git add -A
+  git commit -m "Added comparison files for review"
+  git push
 fi
