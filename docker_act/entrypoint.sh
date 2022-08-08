4a1a52852463d6217dc38d7b7c7ad205946d76e8
marcelmmc <marcel.morancalderon@epfl.ch>
2022-08-08 19:19:41 +0200

    install git in docker

--- docker_act/entrypoint.sh
+++ docker_act/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+pushing_files() {
+  echo "Pushing files"
+  git add -A
+  git commit -m "$1"
+  # git push
+}
+
 echo "Argument being passed: $1"
 # echo "Executing: pip install ms3==0.4.11"
 # pip install ms3==0.4.11
@@ -8,27 +15,23 @@ echo "Executing: cd ${GITHUB_WORKSPACE}/main"
 cd "${GITHUB_WORKSPACE}/main"
 
 if [ "$1" == "extract" ]; then
-  # echo "Executing: ms3 extract -f ${GITHUB_WORKSPACE}/files_modified.json -M -N -X -D"
-  # ms3 extract -f "${GITHUB_WORKSPACE}/files_modified.json" -M -N -X -D
-  # echo "Executing: ms3 extract -f ${GITHUB_WORKSPACE}/files_added.json -M -N -X -D"
-  # ms3 extract -f "${GITHUB_WORKSPACE}/files_added.json" -M -N -X -D
+  echo "Executing: ms3 extract -f ${GITHUB_WORKSPACE}/files_modified.json -M -N -X -D"
+  ms3 extract -f "${GITHUB_WORKSPACE}/files_modified.json" -M -N -X -D
+  echo "Executing: ms3 extract -f ${GITHUB_WORKSPACE}/files_added.json -M -N -X -D"
+  ms3 extract -f "${GITHUB_WORKSPACE}/files_added.json" -M -N -X -D
 
-
-  echo "Pushing files"
-  git add -A
-  git commit -m "Automatically added TSV files from parse with ms3"
-  git push
+  # git add -A
+  # git commit -m "Automatically added TSV files from parse with ms3"
+  # git push
+  pushing_files "Automatically added TSV files from parse with ms3"
 elif [ "$1" == "check"  ]; then
   echo "Executing: ms3 check -f ${GITHUB_WORKSPACE}/files_modified.json --assertion"
   ms3 check -f "${GITHUB_WORKSPACE}/files_modified.json" --assertion
 elif [  "$1" == "compare" ]; then
   echo "Executing: ms3 compare -f ${GITHUB_WORKSPACE}/files_modified.json"
   ms3 compare -f "${GITHUB_WORKSPACE}/files_modified.json"
+
   git config --global user.name "github-actions[bot]"
   git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
-
-  echo "Pushing files"
-  git add -A
-  git commit -m "Added comparison files for review"
-  git push
+  pushing_files "Added comparison files for review"
 fi
