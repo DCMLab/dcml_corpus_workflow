3ce92b82c3c32157fff3174c4aa5a6a9f9e8139a
marcelmmc <marcel.morancalderon@epfl.ch>
2022-08-17 17:59:21 +0200

    update

--- docker_act/entrypoint.sh
+++ docker_act/entrypoint.sh
@@ -85,11 +85,11 @@ get_difference_between_commits(){
 
 main(){
 
-  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-  sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-  sudo apt update
-  sudo apt install gh
+  # curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+  # sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+  # echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+  # sudo apt update
+  # sudo apt install gh
 
   echo "Argument being passed: $1"
   echo "Executing: cd ${GITHUB_WORKSPACE}/main"
