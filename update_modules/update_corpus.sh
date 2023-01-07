4c83a3779f6ce89c46aa7ae0cf60909c9c9f8b26
marcelmmc <marcel.morancalderon@epfl.ch>
2023-01-08 00:27:31 +0100

    new version update

--- update_modules/update_corpus.sh
+++ update_modules/update_corpus.sh
@@ -1,83 +1,55 @@
 #!/bin/bash
-# Ref https://stackoverflow.com/questions/72178053/copy-git-submodules-from-one-repo-to-another
-cd "${GITHUB_WORKSPACE}/main"
-ls -a
-submodules=( $(git config -f "${GITHUB_WORKSPACE}/main/.gitmodules" --name-only --get-regexp 'submodule\..*\.path' | cut -f2 -d.) )
 
-echo "$1"
-for name in "${submodules[@]}"; do
+
+#Global var
+urlToYML="https://github.com/DCMLab/annotation_workflow_template"
+spreedSheetRepos="https://docs.google.com/spreadsheets/d/16UsEfawgln7l9rgG7fTkh_LwO2bg2poLor3SxzXFU0Y/export?exportFormat=csv"
+branch="workflow_update"
+verTemp=$(curl https://api.github.com/repos/DCMLab/dcml_corpus_workflow/releases/latest -s | grep "tag_name"| cut -c16-| head)
+latestVersionDCMLWorkflow=$(echo "${verTemp::-2}")
+
+#Cloning annotation_workflow yml files
+git clone $urlToYML
+cd annotation_workflow_template
+git checkout $branch
+cd ..
+mkdir "${GITHUB_WORKSPACE}/main/update_modules/yml_to_send"
+
+#get the corpora sheet
+cp -r "annotation_workflow_template/.github" "${GITHUB_WORKSPACE}/main/update_modules/yml_to_send"
+curl -L $spreedSheetRepos -o res.csv
+#google sheet contains a  corpora where echa entry contains:
+#the name of a repo, link to the repo and dcml version for the repo
+#[ -n "$name" ] is an extra boolean condition as file does not contain EOF and
+#thus it would not read the last line of this
+while IFS=, read -r path url ver || [ -n "$path" ]
+do
     cd "${GITHUB_WORKSPACE}/main"
-    path="$(git config -f .gitmodules --get submodule."$name".path)"
-    url="$(git config -f .gitmodules --get submodule."$name".url)"
+    echo "$path|$url|$ver"
+    if [[ "$path" == "Repo name" ]]; then
+      # skip the header row
+      continue
+    fi
+    versionSel=$ver
+    if [[ "$ver" == "latest" ]]; then
+      versionSel=$latestVersionDCMLWorkflow
+    fi
+    sed -i "s#uses: DCMLab/dcml_corpus_workflow@..*#uses: DCMLab/dcml_corpus_workflow@$versionSel#" "${GITHUB_WORKSPACE}/main/update_modules/yml_to_send/.github/workflows/main_branch.yml"
+    sed -i "s#uses: DCMLab/dcml_corpus_workflow@..*#uses: DCMLab/dcml_corpus_workflow@$versionSel#" "${GITHUB_WORKSPACE}/main/update_modules/yml_to_send/.github/workflows/annotation_branch.yml"
 
     echo "$url"
     echo "$path"
     linemodified=$(echo "${url/'git@github.com:'/'https://'"$token"'@github.com/'}")
-
-    echo "$url"
-    echo "$path"
     git submodule add "$linemodified" "$path"
+    rm -rf "${GITHUB_WORKSPACE}/main/$path/.github/workflows"
+    cp -r "${GITHUB_WORKSPACE}/main/update_modules/yml_to_send/.github/" "${GITHUB_WORKSPACE}/main/$path/"
+
     cd "${GITHUB_WORKSPACE}/main/$path"
 
-    git branch
     git push origin --delete workflow_update
     git checkout -b workflow_update
     git push --set-upstream origin workflow_update
-    ls -a
-    rm -rf "${GITHUB_WORKSPACE}/main/$path/.github/workflows"
-    cd "${GITHUB_WORKSPACE}/main"
-    ls -a
-    cd "${GITHUB_WORKSPACE}/main/$path"
-    cp -r "${GITHUB_WORKSPACE}/main/update_modules/testing_workflow_helper/.github/" "${GITHUB_WORKSPACE}/main/$path/"
     git add .
     git commit -m "trigger_workflow"
     git push
-
-    #allow some time before asking for the current executed actions
-    sleep 5s
-    gh run list --workflow annotation_branch.yml -L 3 -b workflow_update > res.txt
-
-    echo "garbage" > idrunner.txt
-    while IFS= read -r line
-    do
-      if [[ $line == *"trigger_workflow"* ]]; then
-        echo $line
-        stringarray=($line)
-        if [[ "${stringarray[-1]}" == *"0m" ]]; then
-          echo "${stringarray[-3]}" > idrunner.txt
-          break
-        fi
-      fi
-    done < res.txt
-
-    idrunner=$(cat "idrunner.txt")
-    echo $idrunner
-    runnerdone=$(gh run view $idrunner --json status)
-
-    if [[ ! $idrunner == "garbage" ]]; then
-      while [[ ! "$runnerdone" == *"completed"* ]]
-      do
-        sleep 10s # avoid limit
-                  # api rate 80 calls per minute
-        runnerdone=$(gh run view $idrunner --json status)
-      done
-
-      gh run view "${stringarray[-3]}" --log > res.txt
-      if ! git grep --all-match --no-index -q -e "Executing: ms3 review" "res.txt"; then
-        echo "Error: annotation_branch ran but its workflow-log is not correct"
-        exit 1
-      fi
-    else
-      echo "Error: annotation_branch was not triggered"
-      exit 1
-    fi
-
-
-
-    git pull
-    gh pr create --title "PR to check for errors" --body "This pull request allows reviewers to check for errors before merging to main branch" -B main
-
-    if [[ "$1" == "test" ]]; then
-      break
-    fi
-done
+done < res.csv
